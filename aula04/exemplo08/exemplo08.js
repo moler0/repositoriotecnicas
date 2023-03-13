5cd0d968545be205d937970a29369319e80f4313
Auto Logon <molero>
2023-03-13 10:58:11 -0300

    ultima atualizacao

---
+++ aula04/exemplo08/exemplo08.js
@@ -0,0 +1,6 @@
+var nome  = "molero"
+if (nome == "molero"){
+    var idade = 18  // var escopo global
+    //let idade = 18 // escopo local
+}
+console.log(idade)
---
+++ aula04/exemplo09/script.js
@@ -0,0 +1,4 @@
+function alterar(){
+    document.getElementById("titulo").innerHTML = "trocou o titulo"
+    document.getElementById("campo").value = "trocou o campo"
+}
